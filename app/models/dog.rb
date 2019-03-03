679d8b1e5fe5c96d407f1b7428364ff1c58e905a
Emily Johnson <emily.johnsonmsp@gmail.com>
2019-03-02 18:32:47 -0700

    create  model dog

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,3 +1,9 @@
 class Dog < ApplicationRecord
   belongs_to :owner
+
+  validates :owner_name, length: {2..50}
+  validates :age, numericality: { only_integer: true,
+    greater_than_or_equal_to: 18}, allow_blank: true, on: update
+  validates :dog_name, presence: true on: create
+  validates :city, allow_blank: true
 end
--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,2 +1,8 @@
 class Owner < ApplicationRecord
+  has_many :dogs
+
+  def self.order_owner_name #class method
+    order(:owner_name)
+  end
+
 end
